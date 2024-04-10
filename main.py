b1271fa912eb8c5a84c1117835a4e97e54ecb812
packarmz-git <Marshal.packard22@gmail.com>
2024-04-10 15:10:54 -0400

    wip

--- main.py
+++ main.py
@@ -2,40 +2,46 @@
 import initialize_ACO # Initialize the graph and pheromone matrix
 import ant as Ant # Import ant class definition
 import numpy as np
+import statistics
 
-ITERATIONS = 30
-NUM_ANTS = 30
+ITERATIONS = 500
+NUM_ANTS = 100
 
-ALPHA = 0.7 # Pheromone factor
-BETA = 0.2 # Distance factor
-GAMMA = 0.99 # Evaporation factor
+ALPHA = 3 # Pheromone factor
+BETA = 2 # Distance factor
+GAMMA = 0.9 # Evaporation factor
+STARTINGCITY = 1 # Used when doing single city start
 
 def main():
 
 
     # Create the first colony
-    ants_colony_A = [Ant.Ant(initialize_ACO.distances, initialize_ACO.pheromones, ALPHA, BETA) for _ in range(NUM_ANTS)]
+    ants_colony_A = [Ant.Ant(initialize_ACO.distances, initialize_ACO.pheromones[0], ALPHA, BETA, STARTINGCITY) for _ in range(NUM_ANTS)]
     convergence = []
+    sum_matrix = []
 
     # Calculate tours for each ant for number of iterations
     for iteration in range(ITERATIONS):
+
     
         # Ant Movement
         for ant in ants_colony_A:
             ant.construct_solution()
 
-        # Global Pheromone Update
-        sum_matrix = np.zeros((len(initialize_ACO.distances), len(initialize_ACO.distances[0])))
 
-        # Sum all the matricies from all the ants
+        # sum_matrix = []
+        # City Pheromone Update
+        for _ in initialize_ACO.distances:
+            sum_matrix.append(np.zeros((len(initialize_ACO.distances), len(initialize_ACO.distances[0]))))
+
+        # Sum all the matricies from all the ants for their city
         for ant in ants_colony_A:
-            sum_matrix = sum_matrix + ant.deposited_pheromone_matrix
+            sum_matrix[ant.starting_city] = sum_matrix[ant.starting_city] + ant.deposited_pheromone_matrix
 
-        # Divide by number of ants, so we can change number without modifying alpha
-        sum_matrix = sum_matrix / NUM_ANTS
-
-        # Evaporation
-        updated_pheromones = sum_matrix * GAMMA
+        # Divide by number of ants, so we can change ant count without modifying alpha
+        for k in range(len(sum_matrix)):
+            #sum_matrix[k] = sum_matrix[k] / NUM_ANTS
+            sum_matrix[k] = sum_matrix[k] * GAMMA # Evaporation
 
         total_length = 0
         
@@ -50,14 +56,25 @@ def main():
         #     a.print(index, initialize_ACO.cities)
         #     index += 1
 
+        if iteration == 5 or iteration == 150:
+            ants_colony_A[5].printVariables()
+            ants_colony_A[5].print(5, initialize_ACO.cities)
+
         # Prepare ants for next iteration, pass updated pheromones
         for ant in ants_colony_A:
-            ant.reset(updated_pheromones)
+            ant.reset(sum_matrix[ant.starting_city], STARTINGCITY)
 
-        #if iteration % 5 == 0:
-            # Visualization?
+        # if iteration % 5 == 0:
+        #     print("...")
+        #     print(sum_matrix[1])
+        #     print(ants_colony_A[1].path)
+            
 
     print(convergence) # Print the iterations path length results
+
+    # Print the first 5 and the last 5 elements, averaged to see if there's any improvement
+    print(statistics.mean(convergence[:5]))
+    print(statistics.mean(convergence[-5:]))
 
     # Check for termination criteria (Early convergence, objective function)
     # if termination_criteria_met():
